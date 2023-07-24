0cf5cc816ff7fc004114efd29d0473534921b5e2
Jared-Swanzen <jared@bbd.co.za>
2023-07-24 17:51:23 +0200

    feat: Initial implementation

--- race/index.js
+++ race/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 const app = express();
-const port = 3000;
-const api = 'https://yu7ijviz93.eu-west-1.awsapprunner.com';
+const port = process.env.PORT;
+const api = process.env.API;
+const idp = process.env.idp
 
 const StatusCodes = {
   Okay: 200,
@@ -11,10 +14,40 @@ const StatusCodes = {
 };
 
 app.get('/', (req, res) => {
-  res.send(`<a href="/api/score">score</a><br/><a href="/api/challenge">challenge</a><br/><a href="/api/practice">practice</a>`);
+  res.send(`<a href="/api/score">score</a><br/><a href="/api/challenge">challenge</a><br/><a href="/api/practice">practice</a><br/><a href="${idp}">login</a>`);
+});
+
+app.get('/callback', async (req, res) => {
+
+  const code = req.params?.code;
+
+  if (!code) {
+    res.redirect('/');
+  }
+
+  const response = await fetch('https://pace.auth.eu-west-1.amazoncognito.com/oauth2/token', {
+    method: 'post',
+    body: {
+      grant_type: 'authorization_code',
+      client_id: process.env.IDP_CLIENT_ID,
+      code,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const accessToken = response?.data?.access_token
+
+  console.log('token', accessToken);
+  console.log('response', response);
+
+  res.redirect('/api/score');
 });
 
 app.get('/api/score', async (req, res) => {
+  // TODO: Make all routes do proper catches
+
   const response = await fetch(`${api}/score`);
 
   if (response.status !== StatusCodes.Okay) {
