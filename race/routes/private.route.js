11e2b34bd6e549ef2febac297fee0927308343c4
Gabriel Tidy <gabriel@bbd.co.za>
2023-07-27 15:26:54 +0200

    feat: disallow users from accessing pages of not signed in

--- race/routes/private.route.js
+++ race/routes/private.route.js
@@ -1,22 +1,49 @@
 import { Router } from "express";
 import path from "path";
 import { fileURLToPath } from 'url';
+import { CognitoJwtVerifier } from 'aws-jwt-verify';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const privateRouter = Router();
 
-privateRouter.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../pages/menu.html'));
+const verify = async (idToken, path, fallbackPath = '/../pages/login.html') => {
+    if (!idToken) {
+        return fallbackPath;
+    }
+
+    try {
+        const verifier = CognitoJwtVerifier.create({
+        userPoolId: process.env.USER_POOL_ID,
+        tokenUse: 'id',
+        clientId: process.env.IDP_CLIENT_ID,
+        });
+
+        await verifier.verify(idToken);
+    } catch (error) {
+        return fallbackPath;
+    }
+
+    return path;
+};
+
+privateRouter.get('/', async (req, res) => {
+    const idToken = req?.session?.idToken;
+    const route = await verify(idToken, '/../pages/menu.html');
+    res.sendFile(path.join(__dirname + route));
 });
 
-privateRouter.get('/game', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../pages/index.html'));
+privateRouter.get('/game', async (req, res) => {
+    const idToken = req?.session?.idToken;
+    const route = await verify(idToken, '/../pages/index.html');
+    res.sendFile(path.join(__dirname + route));
 });
 
-privateRouter.get('/leaderboard', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../pages/leaderboard.html'));
+privateRouter.get('/leaderboard', async (req, res) => {
+    const idToken = req?.session?.idToken;
+    const route = await verify(idToken, '/../pages/leaderboard.html');
+    res.sendFile(path.join(__dirname + route));
 });
 
 export default privateRouter;
